9d9664d82a8b1b25a4893f7e842db3bf4a7b4baf
Nebojsa Lukic <nlukic129@gmail.com>
2023-10-02 23:16:32 +0200

    Questions filters

--- src/components/quiz/Quiz.tsx
+++ src/components/quiz/Quiz.tsx
@@ -1,82 +1,76 @@
-import { motion, stagger, useAnimate } from "framer-motion";
+import { motion } from "framer-motion";
 import classes from "./Quiz.module.css";
-import { useEffect, useState } from "react";
+import DropDown from "../ui/DropDown";
+import { useState } from "react";
 
-const staggerMenuItems = stagger(0.1, { startDelay: 0.15 });
+const defaultCategoryText = "All Categories";
+const defaultDifficultyText = "All the difficulties";
+const defaultLimitText = "No Limit Questions";
+
+const categories = [
+  { value: "Default", text: defaultCategoryText },
+  { value: "Linux", text: "Linux" },
+  { value: "DevOps", text: "DevOps" },
+  { value: "Networking", text: "Networking" },
+  { value: "Programming ", text: "Programming " },
+  { value: "Cloud", text: "Cloud" },
+  { value: "Docker", text: "Docker" },
+  { value: "Kubernetes", text: "Kubernetes" },
+];
+
+const difficulties = [
+  { value: "Default", text: defaultDifficultyText },
+  { value: "Easy", text: "Easy" },
+  { value: "Medium", text: "Medium" },
+  { value: "Hard", text: "Hard" },
+];
+
+const limits = [
+  { value: "Default", text: defaultLimitText },
+  { value: "10", text: "10 Questions" },
+  { value: "20", text: "20 Questions" },
+  { value: "30", text: "30 Questions" },
+];
 
 const Quiz = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const scope = useMenuAnimation(isOpen);
+  const [selectedCategory, setSelectedCategory]: any = useState("Default");
+  const [selectedDifficulty, setSelectedDifficulty]: any = useState("Default");
+  const [selectedLimit, setSelectedLimit]: any = useState("Default");
 
-  const [selected, setSelected] = useState("Menu");
+  const [isSelected, setIsSelected]: any = useState(false);
 
-  const selectHandler = (select: any) => {
-    setSelected(select.target.textContent);
+  const selectCategoryHandler = (selectedCategory: string) => {
+    setSelectedCategory(selectedCategory);
+    checkIfSelected();
+  };
+
+  const selectDifficultyHandler = (selectedDifficulty: string) => {
+    setSelectedDifficulty(selectedDifficulty);
+    checkIfSelected();
+  };
+
+  const selectLimitHandler = (selectedLimit: string) => {
+    setSelectedLimit(selectedLimit);
+    checkIfSelected();
+  };
+
+  const checkIfSelected = () => {
+    if (selectedCategory === "Default" && selectedDifficulty === "Default" && selectedLimit === "Default") {
+      setIsSelected(false);
+    } else {
+      setIsSelected(true);
+    }
   };
 
   return (
     <motion.div className={classes["quiz-background"]} initial={{ y: -500, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "just" }}>
-      <motion.nav className={classes.menu} ref={scope} initial={{ y: -200, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
-        <motion.button whileTap={{ scale: 0.97 }} onClick={() => setIsOpen(!isOpen)}>
-          {selected}
-          <div className="arrow" style={{ transformOrigin: "50% 55%" }}>
-            <svg width="15" height="15" viewBox="0 0 20 20">
-              <path d="M0 7 L 20 7 L 10 16" />
-            </svg>
-          </div>
-        </motion.button>
-        <ul
-          style={{
-            pointerEvents: isOpen ? "auto" : "none",
-            clipPath: "inset(10% 50% 90% 50% round 10px)",
-          }}
-        >
-          <li value="Item 1" onClick={selectHandler}>
-            Item 1
-          </li>
-          <li value="Item 2" onClick={selectHandler}>
-            Item 2
-          </li>
-          <li value="Item 3" onClick={selectHandler}>
-            Item 3
-          </li>
-          <li value="Item 4" onClick={selectHandler}>
-            Item 4
-          </li>
-          <li value="Item 5" onClick={selectHandler}>
-            Item 5
-          </li>
-        </ul>
-      </motion.nav>
+      <div className={classes.filters}>
+        <DropDown items={categories} onSelect={selectCategoryHandler} title={defaultCategoryText} />
+        <DropDown items={difficulties} onSelect={selectDifficultyHandler} title={defaultDifficultyText} />
+        <DropDown items={limits} onSelect={selectLimitHandler} title={defaultLimitText} />
+      </div>
     </motion.div>
   );
 };
-
-function useMenuAnimation(isOpen: boolean) {
-  const [scope, animate] = useAnimate();
-
-  useEffect(() => {
-    animate(".arrow", { rotate: isOpen ? 180 : 0 }, { duration: 0.2 });
-
-    animate(
-      "ul",
-      {
-        clipPath: isOpen ? "inset(0% 0% 0% 0% round 10px)" : "inset(10% 50% 90% 50% round 10px)",
-      },
-      {
-        type: "spring",
-        bounce: 0,
-        duration: 0.5,
-      }
-    );
-
-    animate("li", isOpen ? { opacity: 1, scale: 1, filter: "blur(0px)" } : { opacity: 0, scale: 0.3, filter: "blur(20px)" }, {
-      duration: 0.2,
-      delay: isOpen ? staggerMenuItems : 0,
-    });
-  }, [isOpen]);
-
-  return scope;
-}
 
 export default Quiz;
---
+++ src/components/ui/DropDown.tsx
@@ -0,0 +1,81 @@
+import { motion, stagger, useAnimate } from "framer-motion";
+import { useEffect, useState } from "react";
+
+import classes from "./DropDown.module.css";
+
+const staggerMenuItems = stagger(0.1, { startDelay: 0.15 });
+
+const DropDown = ({ items, onSelect, title }: any) => {
+  const [selected, setSelected] = useState(title);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const scope = useMenuAnimation(isOpen);
+
+  const selectHandler = (select: any) => {
+    const selectedItem = select.target.textContent;
+    onSelect(selectedItem);
+    setSelected(selectedItem);
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={classes["drop-down"]}>
+      <motion.nav className={classes.menu} ref={scope} initial={{ y: -200, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
+        <motion.button whileTap={{ scale: 0.97 }} onClick={() => setIsOpen(!isOpen)}>
+          {selected}
+          <div className="arrow" style={{ transformOrigin: "50% 55%" }}>
+            <svg width="15" height="15" viewBox="0 0 20 20">
+              <path d="M0 7 L 20 7 L 10 16" />
+            </svg>
+          </div>
+        </motion.button>
+        <ul
+          style={{
+            pointerEvents: isOpen ? "auto" : "none",
+            clipPath: "inset(10% 50% 90% 50% round 10px)",
+          }}
+        >
+          {items.map((item: any) => (
+            <motion.li
+              value={item.value}
+              onClick={selectHandler}
+              whileHover={{ backgroundColor: "rgb(225, 190, 248)", scale: 1.1, marginRight: 30, cursor: "pointer" }}
+              key={item.value}
+            >
+              {item.text}
+            </motion.li>
+          ))}
+        </ul>
+      </motion.nav>
+    </div>
+  );
+};
+
+export default DropDown;
+
+function useMenuAnimation(isOpen: boolean) {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    animate(".arrow", { rotate: isOpen ? 180 : 0 }, { duration: 0.2 });
+
+    animate(
+      "ul",
+      {
+        clipPath: isOpen ? "inset(0% 0% 0% 0% round 10px)" : "inset(10% 50% 90% 50% round 10px)",
+      },
+      {
+        type: "spring",
+        bounce: 0,
+        duration: 0.5,
+      }
+    );
+
+    animate("li", isOpen ? { opacity: 1, scale: 1, filter: "blur(0px)" } : { opacity: 0, scale: 0.3, filter: "blur(20px)" }, {
+      duration: 0.2,
+      delay: isOpen ? staggerMenuItems : 0,
+    });
+  }, [isOpen]);
+
+  return scope;
+}
